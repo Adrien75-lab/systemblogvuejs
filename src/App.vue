144ddb22a431fb3bd78089f8be46af28c7502343
adrien <adrichristophe@gmail.com>
2020-12-17 20:29:20 +0100

    installation route

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@
   export default {
       name: 'App',
       components: {
-        Slider
+        'contenu': Contenu,
+        'header-top' : Header
       }
     
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vue from './App.vue'
+import App from './App.vue' 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routers:Routes
+})
 
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
